58414c749cbad432921d5209d744a70d89198427
Cihat Yeşildağ <cico643@hotmail.com.tr>
2022-05-06 21:31:06 +0300

    refactor: logger middleware

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -5,13 +5,13 @@ import { NextFunction, Request, Response } from 'express';
 export default class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
   use(request: Request, response: Response, next: NextFunction) {
-    const { ip, method, body } = request;
+    const { ip, method, originalUrl } = request;
     const userAgent = request.get('user-agent') || '';
     response.on('finish', () => {
       const { statusCode } = response;
       const contentLength = response.get('content-length');
       this.logger.log(
-        `${method} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
       );
     });
 
